c17339a1b599ce491f432bf8f0dfe22582c96b32
kiren31 <117424979+kiren31@users.noreply.github.com>
2022-12-01 15:02:54 +0900

    IO
    12.01

--- 11_Exception/src/com/kh/practice/numRange/controller/NumberController.java
+++ 11_Exception/src/com/kh/practice/numRange/controller/NumberController.java
@@ -11,7 +11,7 @@ public class NumberController {
 	public boolean checkDouble(int num1 , int num2) throws NumRangeException {
 		 
 		
-		if(num1 > 0 && num1 < 101 && num2 > 0 && num2 < 101) {
+		if(num1 >= 1 && num1 <= 100 && num2 >= 1 && num2 <= 100) {
 			 if(num1 % num2 == 0) {
 		        return true;
 		         

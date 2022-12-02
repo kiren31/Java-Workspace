fc014e9cd3f8171c1c24e002d24e8f56f978c87d
kiren31 <117424979+kiren31@users.noreply.github.com>
2022-12-02 15:12:37 +0900

    컬렉션
    12.02

---
+++ 12_IO/src/com/kh/practice/score/model/dao/ScoreDAO.java
@@ -0,0 +1,12 @@
+package com.kh.practice.score.model.dao;
+
+public class ScoreDAO {
+	
+	public void saveScore(String name, int kot, int eng, int math, int sum, double avg) {
+		
+	}
+	
+	public DataInputStream readScore throws FileNotFoundException () {
+		
+	}
+}
---
+++ 12_IO/src/com/kh/practice/score/view/ScoreMenu.java
@@ -0,0 +1,10 @@
+package com.kh.practice.score.view;
+
+import java.util.Scanner;
+
+import com.kh.practice.score.controller.ScoreController;
+
+public class ScoreMenu {
+	private Scanner sc = new Scanner(System.in);
+	private ScoreController scr = new ScoreController(); 
+}
